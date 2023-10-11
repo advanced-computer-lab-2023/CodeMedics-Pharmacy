750ddf3e51f86bfc13fc78e3bbb606578e889572
rawanelashmawy <rawanelashmawyy@gmail.com>
2023-10-11 14:25:24 +0300

    addMedicine shaghal

--- controllers/MedicineController.js
+++ controllers/MedicineController.js
@@ -1,12 +1,12 @@
  // Import the Medicine model
  //const MedicineController = require('../controllers/MedicineController');
  //const Medicine = require('../models/Medicine'); 
- const medicineModel = require('../models/Medicine.js');
+ const medicineModel = require('../models/Medicine');
 
  const addMedicine = async (req, res) => {
     try {
         const { name, activeIngredients, price, medicalUse , availableQuantity } = req.body;
-
+        console.log(req.body);
         // Create a new medicine instance
         const newMedicine = new medicineModel({
             name,
@@ -19,15 +19,13 @@
         // Save the medicine to the database
         await newMedicine.save();
 
-        res.status(201).json({ message: 'Medicine added successfully', medicine: newMedicine });
+        res.status(201).json(newMedicine);
     } catch (error) {
         console.error(error); // Log the actual error to the console
         res.status(500).json({ error: 'Error adding medicine' });
     }
 };
 
-module.exports = {
-    addMedicine
-};
+module.exports = {addMedicine};
 
  
