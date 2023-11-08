8619ac1a900538f8282d01b3dc0a3f0de15b6809
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2023-11-08 22:29:46 +0200

    added pharmacist index , medicines page and layouts

---
+++ Frontend/el7a2ny-frontend/src/layouts/dashboard/pharmacist/side-nav.js
@@ -0,0 +1,202 @@
+import NextLink from 'next/link';
+import { usePathname } from 'next/navigation';
+import PropTypes from 'prop-types';
+import ArrowTopRightOnSquareIcon from '@heroicons/react/24/solid/ArrowTopRightOnSquareIcon';
+import ChevronUpDownIcon from '@heroicons/react/24/solid/ChevronUpDownIcon';
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  Stack,
+  SvgIcon,
+  Typography,
+  useMediaQuery
+} from '@mui/material';
+import { Logo } from 'src/components/logo';
+import { Scrollbar } from 'src/components/scrollbar';
+import { items } from './config';
+import { SideNavItem } from './side-nav-item';
+
+export const SideNav = (props) => {
+  const { open, onClose } = props;
+  const pathname = usePathname();
+  const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
+
+  const content = (
+    <Scrollbar
+      sx={{
+        height: '100%',
+        '& .simplebar-content': {
+          height: '100%'
+        },
+        '& .simplebar-scrollbar:before': {
+          background: 'neutral.400'
+        }
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%'
+        }}
+      >
+        <Box sx={{ p: 3 }}>
+          <Box
+            component={NextLink}
+            href="/"
+            sx={{
+              display: 'inline-flex',
+              height: 32,
+              width: 32
+            }}
+          >
+            <Logo />
+          </Box>
+          <Box
+            sx={{
+              alignItems: 'center',
+              backgroundColor: 'rgba(255, 255, 255, 0.04)',
+              borderRadius: 1,
+              cursor: 'pointer',
+              display: 'flex',
+              justifyContent: 'space-between',
+              mt: 2,
+              p: '12px'
+            }}
+          >
+            <div>
+              <Typography
+                color="inherit"
+                variant="subtitle1"
+              >
+                El7a2ny
+              </Typography>
+              <Typography
+                color="neutral.400"
+                variant="body2"
+              >
+                Virtual Pharmacy
+              </Typography>
+            </div>
+          </Box>
+        </Box>
+        <Divider sx={{ borderColor: 'neutral.700' }} />
+        <Box
+          component="nav"
+          sx={{
+            flexGrow: 1,
+            px: 2,
+            py: 3
+          }}
+        >
+          <Stack
+            component="ul"
+            spacing={0.5}
+            sx={{
+              listStyle: 'none',
+              p: 0,
+              m: 0
+            }}
+          >
+            {items.map((item) => {
+              const active = item.path ? (pathname === item.path) : false;
+
+              return (
+                <SideNavItem
+                  active={active}
+                  disabled={item.disabled}
+                  external={item.external}
+                  icon={item.icon}
+                  key={item.title}
+                  path={item.path}
+                  title={item.title}
+                />
+              );
+            })}
+          </Stack>
+        </Box>
+        <Divider sx={{ borderColor: 'neutral.700' }} />
+        <Box
+          sx={{
+            px: 2,
+            py: 3
+          }}
+        >
+          <Typography
+            color="neutral.100"
+            variant="subtitle2"
+          >
+            Need any help?
+          </Typography>
+          <Typography
+            color="neutral.500"
+            variant="body2"
+          >
+            Get instant answers to the most common questions and learn how to use our platform.
+          </Typography>
+          
+          <Button
+            component="a"
+            // endIcon={(
+            //   <SvgIcon fontSize="small">
+            //     <ArrowTopRightOnSquareIcon />
+            //   </SvgIcon>
+            // )}
+            fullWidth
+            // href="https://material-kit-pro-react.devias.io/"
+            sx={{ mt: 2 }}
+            target="_blank"
+            variant="contained"
+          >
+            Contact us
+          </Button>
+        </Box>
+      </Box>
+    </Scrollbar>
+  );
+
+  if (lgUp) {
+    return (
+      <Drawer
+        anchor="left"
+        open
+        PaperProps={{
+          sx: {
+            backgroundColor: 'neutral.800',
+            color: 'common.white',
+            width: 280
+          }
+        }}
+        variant="permanent"
+      >
+        {content}
+      </Drawer>
+    );
+  }
+
+  return (
+    <Drawer
+      anchor="left"
+      onClose={onClose}
+      open={open}
+      PaperProps={{
+        sx: {
+          backgroundColor: 'neutral.800',
+          color: 'common.white',
+          width: 280
+        }
+      }}
+      sx={{ zIndex: (theme) => theme.zIndex.appBar + 100 }}
+      variant="temporary"
+    >
+      {content}
+    </Drawer>
+  );
+};
+
+SideNav.propTypes = {
+  onClose: PropTypes.func,
+  open: PropTypes.bool
+};
