0f394ae234513a28faedbd83db935b5014f8127a
Nour Eldien Ayman <49641430+NourAlPha@users.noreply.github.com>
2023-11-09 19:01:51 +0200

    update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const app = express();
 const Port = process.env.PORT || 3000;
 
