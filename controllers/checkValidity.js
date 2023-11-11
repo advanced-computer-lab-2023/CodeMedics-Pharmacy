9abc0ada4bdf4b0f6ac1757370cc7a51a48f7da2
Omar Mohammad <89214824+OmarMUhammed03@users.noreply.github.com>
2023-11-11 21:32:18 +0200

    fixed bug

--- controllers/checkValidity.js
+++ controllers/checkValidity.js
@@ -5,7 +5,7 @@ const checkValidity = async(req, res) =>{
     const username = getUsername();
     const patient = await Patient.findOne({Username: username});
     const cart = patient.Cart;
-    Boolean ret = true;
+    let ret = true;
     for (let i = 0; i < cart.length; i++) {
         const curMedicine = await Cart.findOne({Name: cart[i].Name});
         if(curMedicine.Quantity < cart[i].Quantity){
