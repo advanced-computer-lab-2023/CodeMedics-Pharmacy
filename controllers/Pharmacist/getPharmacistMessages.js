e3918d27d4f0fd65d5d8d780baa43584e1b0faa3
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2023-12-15 15:44:21 +0200

    added pharmacist notifications

---
+++ controllers/Pharmacist/getPharmacistMessages.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const { getUsername } = require('../../config/infoGetter.js');
+const pharmacistModel = require('../../models/Pharmacist.js');
+
+
+exports.getPharmacistMessages = async (req, res) => {
+    try {
+        const pharmacistUsername = await getUsername(req, res);
+        console.log('Pharmacist username:', pharmacistUsername);
+        // Now you can use pharmacistUsername to retrieve messages
+        const pharmacist = await pharmacistModel.findOne({ Username: pharmacistUsername });
+
+        if (!pharmacist) {
+            console.log('Pharmacist not found.');
+            return res.status(404).json({ error: 'Pharmacist not  found' });
+        }
+
+        console.log('Pharmacist found:', pharmacist);
+
+        res.status(200).json({ Messages: pharmacist.Messages });
+    } catch (error) {
+        console.error('Error retrieving pharmacist messages:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
--- routes/PharmacistRoutes.js
+++ routes/PharmacistRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {registerPharmacist, getPharmacits} = require('../controllers/Pharmacist/PharmacistController');
-
+const {getPharmacistMessages} = require('../controllers/Pharmacist/getPharmacistMessages');
 
 router.post('/register', registerPharmacist);
 router.get('/getPharmacits', getPharmacits);
+router.get('/getPharmacistMessages', getPharmacistMessages);
 
 module.exports = router;
