a3b12f848ffe0735bfe38805da791d31cf8ade4d
mirnahaitham4 <mirnahaitha4@gmail.com>
2023-12-16 22:17:48 +0200

    new email

--- controllers/Pharmacist/MedicineController.js
+++ controllers/Pharmacist/MedicineController.js
@@ -76,15 +76,15 @@ const editMedicine = async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'mirnahaitham2@gmail.com',
-        pass: 'dygc irfq totb kuzy',
+        user: 'codemedics2@gmail.com',
+        pass: 'wwtv oszi mcju tilf',
     },
 });
 
 async function sendEmail(recipient, subject, message) {
     try {
         const mailOptions = {
-            from: 'mirnahaitham2@gmail.com', // Replace with your Gmail email
+            from: 'codemedics2@gmail.com', // Replace with your Gmail email
             to: recipient,
             subject: subject,
             text: message,
