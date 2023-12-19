98805d9e3db341211fe7c2507abc47a5104070d1
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2023-12-19 22:21:54 +0200

    layouts done

--- Frontend/el7a2ny-frontend/src/layouts/dashboard/user/top-nav.js
+++ Frontend/el7a2ny-frontend/src/layouts/dashboard/user/top-nav.js
@@ -3,6 +3,9 @@ import BellIcon from '@heroicons/react/24/solid/BellIcon';
 import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
 import Bars3Icon from '@heroicons/react/24/solid/Bars3Icon';
 import MagnifyingGlassIcon from '@heroicons/react/24/solid/MagnifyingGlassIcon';
+import Link from 'next/link';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { useRouter } from 'next/router';
 import {
   Avatar,
   Badge,
@@ -24,7 +27,7 @@ export const TopNav = (props) => {
   const { onNavOpen } = props;
   const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
   const accountPopover = usePopover();
-
+  const router = useRouter();
   return (
     <>
       <Box
@@ -65,33 +68,17 @@ export const TopNav = (props) => {
                 </SvgIcon>
               </IconButton>
             )}
-            <Tooltip title="Search">
-              <IconButton>
-                <SvgIcon fontSize="small">
-                  <MagnifyingGlassIcon />
-                </SvgIcon>
-              </IconButton>
-            </Tooltip>
+             <IconButton onClick={() => router.back()} size="small" variant="text">
+            <SvgIcon fontSize="medium">
+              <ArrowBackIosNewIcon />
+            </SvgIcon>
+          </IconButton>
           </Stack>
           <Stack
             alignItems="center"
             direction="row"
             spacing={2}
           >
-            <Tooltip title="Contacts">
-              <IconButton>
-                <SvgIcon fontSize="small">
-                  <UsersIcon />
-                </SvgIcon>
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Shopping Cart">
-              <IconButton>
-                  <SvgIcon fontSize="small">
-                    <ShoppingBagIcon />
-                  </SvgIcon>
-              </IconButton>
-            </Tooltip>
             <Avatar
               onClick={accountPopover.handleOpen}
               ref={accountPopover.anchorRef}
